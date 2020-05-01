5005492a1f1737cc9332c6b73ef89682b6f5fa7f
Bijzorg-Tim <admin@bijzorg.com>
2020-05-01 10:34:04 +0200

    added npm install to update script

--- src/update.sh
+++ src/update.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# cd /home/pi/black-mirror-client && git pull && npm install && killall electron && pm2 restart all
-cd /home/pi/black-mirror-client && git pull && killall electron && pm2 restart all
+cd /home/pi/black-mirror-client && git pull && npm install && killall electron && pm2 restart all
