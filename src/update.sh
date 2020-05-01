0a589a9f8f2da211e6d9f62164c32f2954e0ea53
Bijzorg-Tim <admin@bijzorg.com>
2020-05-01 10:25:25 +0200

    update software with scriptturn off npm install

--- src/update.sh
+++ src/update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /home/pi/black-mirror-client && git pull && npm install && killall electron && pm2 restart all
+# cd /home/pi/black-mirror-client && git pull && npm install && killall electron && pm2 restart all
+cd /home/pi/black-mirror-client && git pull && killall electron && pm2 restart all
