2cbf8ac1d7c35bcba45993e0fd4d7543dd273251
Bijzorg-Tim <admin@bijzorg.com>
2020-05-01 22:06:14 +0200

    reenabled python

--- resources/js/store/mutations.js
+++ resources/js/store/mutations.js
@@ -71,71 +71,22 @@ export const setCardsFromServer = (state, payload) => {
 }
 
 export const startCardReadLoop = (state) => {
-    // state.pyshell = null
-    // state.pyshell = new window.PythonShell(window.dirname + '/cardReadLoop.py', { pythonOptions: ['-u']});
-    // console.log('restarting card read loop')
-    // state.pyshell.on('message', function (message) {
-    // // received a message sent from the Python script (a simple "print" statement)
-    //     state.cardRead = message
-    // });
+    state.childProcess.kill('SIGINT');
+    state.pyshell = new window.PythonShell(window.dirname + '/cardReadLoop.py', { pythonOptions: ['-u']});
+    console.log('restarting card read loop')
+    state.pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+        state.cardRead = message
+    });
     
-    // // end the input stream and allow the process to exit
-    // state.pyshell.end(function (err,code,signal) {
-    // if (err) throw err;
-    // });
+    // end the input stream and allow the process to exit
+    state.pyshell.end(function (err,code,signal) {
+    if (err) throw err;
+    });
    
  
 // create an instance of the rpi-mfrc522 class using the default settings
-let mfrc522 = new RPiMfrc522();
- 
-// initialize the class instance then start the detect card loop
-mfrc522.init()
-  .then(() => {
-    loop();
-  })
-  .catch(error => {
-    console.log('ERROR:', error.message)
-  });
- 
- 
-// loop method to start detecting a card
-function loop () {
-  console.log('Loop start...');
-  cardDetect()
-    .catch(error => {
-      console.log('ERROR', error.message);
-    });
-}
- 
- 
-// delay then call loop again
-function reLoop () {
-  setTimeout(loop, 25);
-}
 
-function uidToString(uid) {
-    return uid.reduce((s, b) => { return s + (b < 16 ? '0' : '') + b.toString(16); }, '');
-  }
- 
- 
-// call the rpi-mfrc522 methods to detect a card
-async function cardDetect () {
-  // use the cardPresent() method to detect if one or more cards are in the PCD field
-  if (!(await mfrc522.cardPresent())) {
-    console.log('No card')
-    return reLoop();
-  }
-  // use the antiCollision() method to detect if only one card is present and return the cards UID
-  let uid = await mfrc522.antiCollision();
-  if (!uid) {
-    // there may be multiple cards in the PCD field
-    console.log('Collision');
-    return reLoop();
-  }
-  console.log('Card detected, UID ' + uidToString(uid));
-  await mfrc522.resetPCD()
-  reLoop();
-}
 }
 
 export const addDeviceStatusToDeviceConfig = (state, payload) => {
